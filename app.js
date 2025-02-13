cb6fffc6581a33b749c1632bf3d6da0ed8e6b058
Paulo Henrique Valadão <paulohsvaladao@gmail.com>
2025-02-13 20:01:13 -0300

    primeiras funções

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [] // Array global para armazenar os amigos
+
+function adicionarAmigo() {
+    let inputAmigo = document.getElementById("amigo"); // Captura o input pelo ID
+    let nome = inputAmigo.value.trim(); // Captura o valor digitado e remove espaços extras
+
+    if (nome === '') {
+        alert('Por favor, insira um nome.');
+        return; // Interrompe a função se o campo estiver vazio
+    }
+    if (amigos.includes(nome)) {
+        alert('Esse nome já foi adicionado.');
+        return; // Interrompe a função se o nome já existir
+    }
+    amigos.push(nome); // Adiciona o nome ao array
+    atualizarLista(); // Chama a função que atualiza a lista na tela
+    inputAmigo.value = ''; // Limpa o campo de entrada
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos'); // Captura a <ul> da lista
+    lista.innertHTML = ''; // Limpa a lista antes dea atualizar
+
+    amigos.forEach(function (amigo) {
+        let li = document.createElement('li'); // Cria um novo <li>
+        li.textContent = amigo; // Define o nome dentro do <li>
+        lista.appendChild(li); // Adiciona o <li> na <ul>
+    });
+}
